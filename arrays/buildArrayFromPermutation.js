9e0a531805837bdfc45845d154b93bd889a123ec
Flu3nt <gnedyalkov94@gmail.com>
2023-08-12 17:34:40 +0300

    restructure folder

---
+++ arrays/buildArrayFromPermutation.js
@@ -0,0 +1,13 @@
+function buildArray(nums) {
+    let result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        result.push(nums[nums[i]]);
+    }
+
+    return result;
+}
+
+let arr = [0, 2, 1, 5, 3, 4];
+
+buildArray(arr);
