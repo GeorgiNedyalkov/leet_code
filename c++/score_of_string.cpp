bb603797571c2b145fffda11dff5f58cbc92848d
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-08-09 15:33:04 +0300

    solve score of strings problem

---
+++ c++/score_of_string.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+// Input: is a string
+// Output: int score
+
+// A score of a string is the sum of the absolute difference of adjacent ASCII characters in the string.
+
+int scoreOfString(std::string s)
+{
+    int score = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (i + 1 == s.size())
+        {
+            break;
+        }
+
+        score += fabs(s[i] - s[i + 1]);
+    }
+
+    std::cout << score << std::endl;
+    return score;
+}
+
+int main()
+{
+    std::string evenInput = "hello";
+    std::string oddInput = "zaz";
+
+    scoreOfString(evenInput);
+    scoreOfString(oddInput);
+
+    return 0;
+}
